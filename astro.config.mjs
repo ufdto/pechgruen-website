8de95d22a6bb6aafdad34417a5735ebb4a2fb404
Udo Dengler <udo_dengler@yahoo.com>
2026-01-03 10:10:07 -0500

    Affenfelsen: Bestimmung der Koordinaten mit Hilfe eines Ludtbild und Karten-Overlays erklaert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   site: "https://ufdto.github.io",
   base: "/",
+  integrations: [mdx()],
 });
